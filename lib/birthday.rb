05bdaf4ac473b29b1a1c36d0ac3a21cc7941db32
Wills Blake <richwblake@gmail.com>
2019-11-07 02:44:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,6 +10,6 @@ end
 
 def age_appropriate_birthday( birthday_kids )
   birthday_kids.each { |key, value| 
-    value <= 12 ? "Happy Birthday #{key}! You are now #{value} years old!" : "You are too old for this"
+    puts value <= 12 ? "Happy Birthday #{key}! You are now #{value} years old!" : "You are too old for this"
   }
 end
